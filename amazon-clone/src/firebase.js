7d3fb5c27d4eee34b3e8b1e8eb8216004b7e5411
andrewc0urt <andrew.court.dev@gmail.com>
2024-01-03 18:41:18 -0500

    Initial Project Setup
    Set up the initial project structure using "npx create-react-app amazon-clone"
    
    'npx' is package runner tool that comes with npm. It is used to execute binaries from npm packages.
    
    'create-react-app' is a command-line utility that sets up a new React project with a recommended project structure and initial configurations. It simplifies the process of creating a new React application
    
    The full command fetches the latest version of 'create-react-app' from the npm registry, uses it to create a new React project in a directory named 'amazon-clone', and sets up the necessary files and dependencies for a basic React application.
    
    Also added a firebase.js file with code for setting up the Firebase DB.

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBPfPHvZzjKEWDo_MoR1JMbdGU2Vstrn18",
+  authDomain: "clone-firebase-8af09.firebaseapp.com",
+  projectId: "clone-firebase-8af09",
+  storageBucket: "clone-firebase-8af09.appspot.com",
+  messagingSenderId: "1002676489745",
+  appId: "1:1002676489745:web:9e95ad1a530bda3aacebb0",
+  measurementId: "G-KLZLWY0T6Z"
+};

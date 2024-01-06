614882e301a9a39c22510eb6f26d303efd24d88f
andrewc0urt <andrew.court.dev@gmail.com>
2024-01-06 16:53:48 -0500

    Completed Homepage Setup, Styling, and Routing
    Implemented and styled the homepage to mimic the Amazon homepage, adding product displays. This is purely frontend work, with no backend functionality at this point.
    
    - Completed homepage layout and added products
    - Styled the homepage to match the Amazon design
    - Set up routes for home, checkout and a wildcard route for handling 404 errors
    - Utilized mui.com for React templates, icons, buttons, etc.
    - Installed 'react-router-dom' via 'npm install' for frontend routing functionality

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,15 +1,45 @@
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
 
 function App() {
 	return (
-		// BEM (naming convention - Block, Element, Modifier)
-		// renamed "App" to "app"
-		<div className="app">
-			{/* Import Header.js into the App */}
-			<Header />
-			{/* Home */}
-		</div>
+		<Router>
+			{/* BEM (naming convention - Block, Element, Modifier) renamed "App" to"app" */}
+			<div className="app">
+				<Routes>
+					{/* Route for Checkout Page */}
+					<Route
+						path="/checkout"
+						element={
+							//Wrap the multiple elements in a fragment tag
+							<>
+								<Header /> <h1>This is the checkout page!</h1>
+							</>
+						}
+					></Route>
+
+					{/* Default Route to Homepage */}
+					<Route
+						path="/"
+						element={
+							<>
+								<Header /> <Home />
+							</>
+						}
+					></Route>
+
+					{/* Wildcard route for handling invalid routes or 404 errors */}
+					<Route path="*" element={<Navigate to="/" />}></Route>
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 

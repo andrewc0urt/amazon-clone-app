68abd5ebeb48078565d1a868a550f5fd5313bc19
andrewc0urt <andrew.court.dev@gmail.com>
2024-01-07 10:46:02 -0500

    Display Shopping Cart Items Count and Subtotal Dynamically on Checkout Page
    Enhance the Subtotal.js component to dynamically display the number of items in the shopping cart and the current subtotal on the checkout page.
    
    - Utilize a selector to retrieve the count of items in the shopping cart from the global state.
    - Implement the dynamic display of the cart items count and subtotal in the Subtotal.js component.
    - Ensure accurate formatting and presentation using the `CurrencyFormat` component.

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -1,16 +1,27 @@
 import React from "react";
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+import { getShoppingCartTotal } from "./reducer";
 
 function Subtotal() {
+	const [{ basket }, dispatch] = useStateValue();
+
+	// Get the total price of all items in the shopping cart
+	let cart_subtotal = 0;
+
+	for (let item = 0; item < basket.length; item++) {
+		cart_subtotal += basket[item].price;
+	}
+
 	return (
 		<div className="subtotal">
 			<CurrencyFormat
 				renderText={(value) => (
 					<>
 						<p>
-							Subtotal (0 items):
-							<strong>$0.00</strong>
+							Subtotal ({basket?.length} items):
+							<strong>{value}</strong>
 						</p>
 						<small className="subtotal_gift">
 							<input type="checkbox" />
@@ -19,7 +30,8 @@ function Subtotal() {
 					</>
 				)}
 				decimalScale={2} // Go to 2 decimal places
-				value={0} // Current amount being passed in
+				// value={cart_subtotal} // Current amount being passed in
+				value={getShoppingCartTotal(basket)}
 				displayType={"text"}
 				thousandSeparator={true}
 				prefix={"$"}
--- amazon-clone/src/reducer.js
+++ amazon-clone/src/reducer.js
@@ -4,6 +4,11 @@ export const initialState = {
 	basket: [],
 };
 
+// Selector to get the total price of all items in the shopping cart
+// Using a selector is a better practice than a for loop (as seen in Subtotal.js - currently commented out)
+export const getShoppingCartTotal = (basket) =>
+	basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
 	console.log(action);
 
